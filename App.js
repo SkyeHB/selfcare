9ad0422b992b2190e20c87089a29ef41a8b89724
Xalandria <maciver_scott@gmail.com>
2021-06-10 00:25:05 -0400

    fixed routing, added login and profile pages, created navbar

--- App.js
+++ App.js
@@ -1,6 +1,55 @@
-import React from "react";
+import 'react-native-gesture-handler';
+import React, { Profiler } from "react";
 import Welcome from "./app/screens/Welcome";
+import Login from "./app/screens/Login";
+import Profile from "./app/screens/Profile";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
+import { createAppContainer } from 'react-navigation';
+import { Appbar, Menu } from 'react-native-paper';
+import { Provider as PaperProvider} from 'react-native-paper'
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return <Welcome />;
+  return (
+    <PaperProvider>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Welcome"
+        screenOptions={{
+          header: (props) => <CustomNavigationBar {...props} />,
+        }}>
+    <Stack.Screen name="Welcome" component={Welcome} />
+    <Stack.Screen name="Login" component={Login} />
+    <Stack.Screen name="Profile" component={Profile} />
+    </Stack.Navigator>
+  </NavigationContainer>
+  </PaperProvider>
+  );
+}
+
+function CustomNavigationBar({ navigation, previous }) {
+  const [visible, setVisible] = React.useState(false);
+  const openMenu = () => setVisible(true);
+  const closeMenu = () => setVisible(false);
+
+  return (
+    <Appbar.Header>
+      {previous ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
+      <Appbar.Content title="SelfCare" />
+      {!previous ? (
+        <Menu
+          visible={visible}
+          onDismiss={closeMenu}
+          anchor={
+            <Appbar.Action icon="menu" color="white" onPress={openMenu} />
+          }>
+          <Menu.Item onPress={() => {console.log('Option 1 was pressed')}} title="Option 1" />
+          <Menu.Item onPress={() => {console.log('Option 2 was pressed')}} title="Option 2" />
+          <Menu.Item onPress={() => {console.log('Option 3 was pressed')}} title="Option 3" disabled />
+        </Menu>
+      ) : null}
+    </Appbar.Header>
+  );
 }
--- app/screens/Login.js
+++ app/screens/Login.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { render } from "react-dom";
 import {
   StyleSheet,
   Text,
+  TextInput,
   View,
   Image,
   TouchableOpacity,
@@ -10,22 +12,42 @@ import {
 } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Login from "./Login";
 
-function Welcome({navigation}) {
+class Login extends React.Component{
+constructor(props){
+  super(props);
+  this.state = {
+    email: "",
+    pass: ""
+  };
+  this.authenticateUser = this.authenticateUser.bind(this);
+}
+render(props) {
+
   return (
     <SafeAreaView style={styles.container}>
       <TouchableOpacity>
         <Image fadeDuration={3000} source={require("../assets/d.png")}></Image>
       </TouchableOpacity>
       <View style={styles.loginsignup}>
-        <Button title="Log In" onPress={() => navigation.navigate('Login')}/>
-        <Button title="Sign Up" />
+        <View>
+            <TextInput placeholder="Email" name="email" onChangeText={data => this.setState({email:data})} />
+            <TextInput placeholder="password" name="password" secureTextEntry={true} onChangeText={data => this.setState({pass: data})}/>
+            <Button title="Log In" onPress={() => this.authenticateUser()}/>
+        </View>
       </View>
     </SafeAreaView>
   );
 }
+authenticateUser(navigation){
 
+console.log(this.state.pass)
+console.log(this.state.email);
+this.props.navigation.navigate('Profile');
+
+}
+
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -38,5 +60,4 @@ const styles = StyleSheet.create({
     backgroundColor: "#a5c1e5",
   },
 });
-
-export default Welcome;
+export default Login;
